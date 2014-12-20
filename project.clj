472f3afba28fe378eaa43eeef5fb543365210f9a
unionx <unionx@foxmail.com>
2014-12-20 23:44:02 +0800

    Change homepage and favicon

--- project.clj
+++ project.clj
@@ -4,8 +4,11 @@
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.3.1"]
+                 [selmer "0.7.7"]
+                 [environ "1.0.0"]
                  [ring/ring-defaults "0.1.2"]]
-  :plugins [[lein-ring "0.8.13"]]
+  :plugins [[lein-ring "0.8.13"]
+            [lein-environ "1.0.0"]]
   :ring {:handler genisland.core.handler/app
          :uberwar-name "genisland.war"
          :auto-reload? true
--- src/genisland/core/handler.clj
+++ src/genisland/core/handler.clj
@@ -1,10 +1,14 @@
 (ns genisland.core.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
+            [genisland.core.layout :as layout]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
+(defn home-page []
+  (layout/render "index.html"))
+
 (defroutes app-routes
-  (GET "/" [] "Hello, the island.")
+  (GET "/" [] (home-page))
   (route/not-found "Not Found"))
 
 (def app
